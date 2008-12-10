211e0f0401a34baccdd5ae6d435feb7e0e96e4dc
handk85 <handk85@4fabb465-0037-0410-8b86-c3efdef5e10a>
2008-12-10 11:36:27 +0000

    start handk's project
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@255 4fabb465-0037-0410-8b86-c3efdef5e10a

---
+++ oss2008cjnu/handk/index.php
@@ -0,0 +1,31 @@
+<?php
+   function extractKeyword($target, $mother) {
+       global $configVal;
+       requireComponent('Textcube.Function.Setting');
+       $data = setting::fetchConfigVal( $configVal);
+	   
+	   if(is_null($data)){
+		   return $target."API 키를 입력해주세요";
+	   }else{
+		   $apikey=$data['apikey'];
+	   }
+	   $request="http://apis.daum.net/suggest/keyword?apikey=".$apikey."&q='".$target."'";
+	   $response = file_get_contents($request);
+	   $obj = simplexml_load_string($response);      
+
+	   $daum=$obj->daum;
+	   $printer="";
+	   foreach($daum->item as $value){
+		   $printer.=" ".$value->keyword;
+	   }
+	   
+       $target=$target."<div class='listbox'><h3>Extracted Keywords</h3><div>$printer</div></div>";
+
+       return $target;
+   }
+   function keywordDataSet($DATA){
+       requireComponent('Textcube.Function.Setting');
+       $cfg = setting::fetchConfigVal( $DATA );
+       return true;
+   }
+   ?>
