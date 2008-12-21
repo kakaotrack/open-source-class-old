5780d89c35d1e49a1bb1ab0237ea52a8364b9ab2
kimyj88 <kimyj88@4fabb465-0037-0410-8b86-c3efdef5e10a>
2008-12-21 14:43:30 +0000

    이벤트핸들러를 이용한 맵 띄우기 진행중
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@279 4fabb465-0037-0410-8b86-c3efdef5e10a

---
+++ DMap/config.php
@@ -0,0 +1,15 @@
+<?
+# openapi keys
+$daumkey = "657583606f04bc4f455035664df326869c4c79b7";
+
+# starting coordinates in DMap popup.
+$view = "N"; 
+$zoom = 6; 
+$b_x = 306976; 
+$b_y = 547680; 
+
+# plugin URL
+$path = "/blog";
+$NGMappluginURL = $path."/plugins/DMap";
+
+?>
--- DMap/index.php
+++ DMap/index.php
@@ -1,9 +1,20 @@
-<?php
-     function Helloworld_Show($target) {
-          return $target.'Insert Map';
-     }
+<?
+require "config.php";
 
-	 function Test_Show($target){
-		 return $target.'test';
-	 }
+function DMap_AddPostEditorToolBox($target, $mother) {
+
+	global $pluginURL, $service;
+
+	$urlprefix = "http://".$service['domain'].$pluginURL;
+
+	$content.= "
+	<script language='javascript'>        
+	function openDMap() {
+		window.open('http://117.17.102.230/blog/plugins/DMap/DMap_show.html','DMap','width=900,height=600,scrollbars=no,resizable=yes');
+	}
+	</script>
+	<img onclick='openDMap()' style='padding: 2px;' class='pointerCursor' src='http://117.17.102.230/blog/plugins/DMap/get_map.gif' /><br/>
+	";
+	return $content.$target;
+}
 ?>
