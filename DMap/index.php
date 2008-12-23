cbb3063817c5cdaad801fbf06428fc1c6dc5fe4d
kimyj88 <kimyj88@4fabb465-0037-0410-8b86-c3efdef5e10a>
2008-12-23 19:33:19 +0000

    다음맵 연결을 위한 파일 분리(기본파일 제외한 파일의 삭제)
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@288 4fabb465-0037-0410-8b86-c3efdef5e10a

--- DMap/index.php
+++ DMap/index.php
@@ -2,13 +2,12 @@
 function DMap_AddPostEditorToolBox($target, $mother) {
 
 	global $pluginURL, $service;
-
 	$urlprefix = "http://".$service['domain'].$pluginURL;
 
 	$content= "
 	<script language='javascript'>        
 	function openDMap() {
-		window.open('http://117.17.102.230/map/DMap_show.php','DMap','width=900,height=600,scrollbars=no,resizable=yes');
+		window.open('http://117.17.102.230/DMap_e/DMap.php','DMap','width=900,height=600,scrollbars=no,resizable=yes');
 	}
 	</script>
 	<img onclick='openDMap()' style='padding: 2px;' class='pointerCursor' src='http://117.17.102.230/map/get_map.gif' /><br/>
