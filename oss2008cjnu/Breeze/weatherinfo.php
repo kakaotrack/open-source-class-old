f5781ebfeb0f2f08b3689e7c37ebbaf39ae0c6a1
98breeze <98breeze@4fabb465-0037-0410-8b86-c3efdef5e10a>
2008-12-25 16:35:28 +0000

    날씨 선택도시 툴 추가, 파싱 데이터 작성 완료
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@307 4fabb465-0037-0410-8b86-c3efdef5e10a

--- oss2008cjnu/Breeze/weatherinfo.php
+++ oss2008cjnu/Breeze/weatherinfo.php
@@ -1,24 +1,32 @@
 <?php
 
+include "cityinfo.php";
 
-$weather = "http://www.google.co.kr/ig/api?&hl=en&weather=Jeju";
+$temp = $_POST["city_info"];
+$cityinfo = "city_info[temp]";
+
+
+$weather = "http://www.google.co.kr/ig/api?&hl=en&weather=".$cityinfo;
 $img = "http://www.google.co.kr";
 
 $xml = simplexml_load_file($weather)->weather->forecast_information;
 $xml1 = simplexml_load_file($weather)->weather->current_conditions;
+$xml2 = simplexml_load_file($weather)->weather->forecast_conditions;
+
 $city = $xml->city;
 $date = $xml->forecast_date;
 $condition = $xml1->condition;
-$temp_c = $xml1->temp_c;
+$temp_min = $xml2->low;
+$temp_max = $xml2->high;
 $humidity = $xml1->humidity;
 $imgdata = $xml1->icon;
 $wind = $xml1->wind_condition;
 
 
-
+/*
 echo "<xmp>"; 
-print_r($xml1); 
+print_r($date); 
 echo "</xmp>"; 
-
+*/
 
 ?>
