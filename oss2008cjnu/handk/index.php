88dffd06f19628258deead79c33829c3c5be9f5c
handk85 <handk85@4fabb465-0037-0410-8b86-c3efdef5e10a>
2008-12-25 13:58:13 +0000

    swf경로를 정적에서 동적으로 변경
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@303 4fabb465-0037-0410-8b86-c3efdef5e10a

--- oss2008cjnu/handk/index.php
+++ oss2008cjnu/handk/index.php
@@ -1,4 +1,5 @@
 <?php
+   include("config.php");
    function extractKeyword($target, $mother) {
        global $configVal;
        requireComponent('Textcube.Function.Setting');
@@ -8,15 +9,16 @@
 		   return $target."API 키를 입력해주세요";
 	   }else{
 		   $apikey=$data['apikey'];
-	   }
+	   } $plugInURL="http://".getenv("SERVER_NAME").$_SERVER["REQUEST_URI"]."plugins/DKKey";
 
 	   $query=str_replace("&"," ",$target);
 	   $query=str_replace("'", "",$query);
 	   
 	   $target=$target."<div id='listbox' class='listbox'><h3>Extracted Keywords</h3>";
 
-	   $target=$target."<center><object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' id='KeywordExtractor' width='400' height='150' codebase='http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab'> <param name='movie' value='http://handkstory.net/~handk/plugins/DKKey/KeywordExtractor.swf' /><param name='quality' value='high' /><param name='bgcolor' value='#ffffff'/><param name='allowScriptAccess' value='sameDomain'/><embed src='http://handkstory.net/~handk/plugins/DKKey/KeywordExtractor.swf?apikey=$apikey&query=$query' quality='high' bgcolor='#ffffff' width='400' height='150' name='KeywordExtractor' align='middle'	play='true' loop='false' quality='high' allowScriptAccess='sameDomain' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer'></embed></object></center>";
+	   $target=$target."<center><object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' id='KeywordExtractor' width='400' height='150' codebase='http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab'> <param name='movie' value='$plugInURL/KeywordExtractor.swf' /><param name='quality' value='high' /><param name='bgcolor' value='#ffffff'/><param name='allowScriptAccess' value='sameDomain'/><embed src='$plugInURL/KeywordExtractor.swf?apikey=$apikey&query=$query' quality='high' bgcolor='#ffffff' width='400' height='150' name='KeywordExtractor' align='middle'	play='true' loop='false' quality='high' allowScriptAccess='sameDomain' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer'></embed></object></center>";
 	   $target=$target."<br>powered by<img src='http://deco.daum-img.net/service_title/logo.gif' height='20'/ valign='middle'></div>";
+
 	   return $target;
 	}
 	/* Flex Component start*/
