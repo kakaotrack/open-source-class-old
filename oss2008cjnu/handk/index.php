79d2632c4473cd24fc662e3e0b9910f4b32509ef
handk85 <handk85@4fabb465-0037-0410-8b86-c3efdef5e10a>
2008-12-25 11:07:44 +0000

    SWF와 Javascript간 변수전달 구현 완료. 1차 기능 구현 완료. 확인 페이지 : http://handkstory.net/~handk
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@297 4fabb465-0037-0410-8b86-c3efdef5e10a

--- oss2008cjnu/handk/index.php
+++ oss2008cjnu/handk/index.php
@@ -9,40 +9,12 @@
 	   }else{
 		   $apikey=$data['apikey'];
 	   }
-?>
-<!--
-<script type="text/javascript">
- function start(x,y){
-	alert(x+","+y);
-	getKey();
-	getQuery();
-}
- function getKey() {
-	 mySWF = document.getElementById("${application}");
-     mySWF.getKey("<? echo $apikey; ?>");
- }
- function getQuery() {
-	 mySWF = document.getElementById("${application}");
-     mySWF.getQuery("<? echo $target; ?>");
- }
-</script>-->
-<?
+	   $query=str_replace("&"," ",$target);
+	   //$query=$target;
 	   $target=$target."<div id='listbox' class='listbox'><h3>Extracted Keywords</h3>";
-/*
-	   $target=$target."<script language='JavaScript' type='text/javascript'>";
-	   $target=$target."function start(x,y){";
-	   $target=$target."alert(x+','+y);";
-	   $target=$target."getKey();";
-	   $target=$target."getQuery();}";
-       $target=$target."function getKey(){";
-	   $target=$target."mySWF = document.getElementByID('KeywordExtractor');";
-	   $target=$target."mySWF.getKey('$apikey');}";
-	   $target=$target."function getQuery(){";
-	   $target=$target."mySWF = document.getElementByID('${application}');";
-	   $target=$target."mySWF.getQuery('".$target."');}</script>";
-*/
-		$query=preg_replace("/<>\#*","",$target);
-	   $target=$target."<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' id='KeywordExtractor' width='400' height='250' codebase='http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab'> <param name='movie' value='http://handkstory.net/~handk/plugins/DKKey/KeywordExtractor.swf' />	<param name='quality' value='high' /><param name='bgcolor' value='#ffffff'/><param name='allowScriptAccess' value='sameDomain'/><embed src='http://handkstory.net/~handk/plugins/DKKey/KeywordExtractor.swf?apikey=$apikey&query=$query' quality='high' bgcolor='#ffffff' width='400' height='250' name='KeywordExtractor' align='middle'	play='true' loop='false' quality='high' allowScriptAccess='sameDomain' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer'></embed></object></div>";
+
+	   $target=$target."<center><object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' id='KeywordExtractor' width='400' height='150' codebase='http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab'> <param name='movie' value='http://handkstory.net/~handk/plugins/DKKey/KeywordExtractor.swf' /><param name='quality' value='high' /><param name='bgcolor' value='#ffffff'/><param name='allowScriptAccess' value='sameDomain'/><embed src='http://handkstory.net/~handk/plugins/DKKey/KeywordExtractor.swf?apikey=$apikey&query=$query' quality='high' bgcolor='#ffffff' width='400' height='150' name='KeywordExtractor' align='middle'	play='true' loop='false' quality='high' allowScriptAccess='sameDomain' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer'></embed></object></center>";
+	   $target=$target."<br>powered by<img src='http://deco.daum-img.net/service_title/logo.gif' height='20'/ valign='middle'></div>";
 	   return $target;
 	}
 	/* Flex Component start*/
