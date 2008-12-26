541b3585d28007a44d1e9ed28f1ceb4baacd5ad7
lucael4u <lucael4u@4fabb465-0037-0410-8b86-c3efdef5e10a>
2008-12-26 17:40:27 +0000

    잘못 알고 있던 이벤트리스너 사용법을 알아내어 XML 파일에 이벤트 리스너(본문) 추가했습니다. index.php은 JSON.PHP파일을 인클루드 시켜 JSON값을 받아와서 PHP에 저장하는데까지 구현중입니다
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@319 4fabb465-0037-0410-8b86-c3efdef5e10a

--- oss2008cjnu/KeywordLink/index.php
+++ oss2008cjnu/KeywordLink/index.php
@@ -1,4 +1,6 @@
 <?php
+   include("config.php");
+
 /* KeywordUI for Textcube 1.76
    ----------------------------------
    Version 1.5
@@ -26,22 +28,53 @@
 //http://openapi.naver.com/search?key=6484ff3113728f5c49e7d921205d61a1&target=krdic&query=%EC%98%81%EC%96%B4&start=1&display=10
 
 //$apiurl = "http://openapi.naver.com/search";
-//$apikey = "6484ff3113728f5c49e7d921205d61a1";
+
+
+function KeywordLink_insTB($target,$mother) {
+
+	requireComponent('Textcube.Function.misc');
+
+//	echo "target : ".$target."<br />";
+//	echo "mother : ".$mother."<br />";
+
+	$data = setting::fetchConfigVal( $configVal);
+	
+
+
+
+	// 본문 키워드 추출후 카운트 갯수에 따라 테이블 줄수를 증가시켜 출력
+	$target = $target."";
+
+	return $target;
+}
+
+function KeywordLink_insJS($target,$mother) {
+
+
+
+?>
+<script>
+
+</script>
+
+
+<?
+
+
+
+}
 
 
 function KeywordLink_bindKeyword($target,$mother) { //팝업 띄우면서 넘겨주는 부분
 	global $blogURL;
+	global $pluginPath;
 	global $configVal;
-
-//	$apikey = "6484ff3113728f5c49e7d921205d61a1";
 
 	requireComponent('Textcube.Function.misc');
 	$data = setting::fetchConfigVal( $configVal);
 	
-	$apikey=$data['apikey'];
-	
-	echo $blogURL.' => '.$apikey;
-
+	echo $target." " ;
+//	$apikey=$data['apikey'];
 /*
 	if(is_null($data)){
 		return $target." API 키를 입력하세요 ";
@@ -49,37 +82,10 @@ function KeywordLink_bindKeyword($target,$mother) { //팝업 띄우면서 넘겨
 	else{
 		$apikey=$data['apikey'];
 	}
-*/
-//	$target = "<a href=\"#\" class=\"key1\" onclick=\"openKeyword('$blogURL/keylog/" . rawurlencode($target) . "'); return false\">{$target}</a>";
+
+	$target = "<a href=\"#\" class=\"key1\" onclick=\"openKeyword('$blogURL/keylog/" . rawurlencode($target) . "'); return false\">{$target}</a>";
+
 /*
-<table>
-  <tr>
-    <td align='center' onmouseover="menulayer_bgroup1.style.display='';" onmouseout="menulayer_bgroup1.style.display='none';" >
-
-      <table border='0' cellspacing='0' cellpadding='0'  width='100%' bgcolor="#cccccc">
-        <tr>
-	      <td  align='center'>
-      		<a href='javascript://' class='top_menu'>키워드</a>
-	      </td>
-		</tr>
-		<tr>
-		  <td align='center'>
-            <div id='menulayer_bgroup1' style='margin-top:-2px; margin-left:-30px; display:none; position:absolute;'>
-        		<table width='60' border='0' cellspacing='0' cellpadding='0' onmouseover=menulayer_bgroup1.style.display='';  onmouseout=menulayer_bgroup1.style.display='none'; >
-			       <tr>
-				      <td style='padding-top:5px;'>검색리스트</td>
-		          </tr>
-	            </table>
-		    </div>
-    	  </td>
-	    </tr>
-	  </table>
-      
-	</td>
-  </tr>
-</table>
-*/
-
 	$target = "
 <table>
 	<tr>
@@ -87,16 +93,16 @@ function KeywordLink_bindKeyword($target,$mother) { //팝업 띄우면서 넘겨
 			<table border='0' cellspacing='0' cellpadding='0'  width='100%' bgcolor=\"#cccccc\">
 				<tr>
 					<td  align='center'>
-						<a href=\"http://openapi.naver.com/search?key=".$apikey."&target=krdic&start=1&display=10&query=". rawurlencode($target) ."\" class= \" key1 \"  return false\">{$target}</a></td>
-</td>
-		</tr>
-		<tr>
-		  <td align='center'>
-            <div id='menulayer_bgroup1' style='margin-top:-2px; margin-left:-30px; display:none; position:absolute;'>
-        		<table width='60' border='0' cellspacing='0' cellpadding='0' onmouseover=menulayer_bgroup1.style.display='';  onmouseout=menulayer_bgroup1.style.display='none'; >
-			       <tr>
-				      <td style='padding-top:5px;'>검색리스트<? include 'config.php'; ?></td>
-		          </tr>
+						<a href=\"http://openapi.naver.com/search?key=".$apikey."&target=krdic&start=1&display=10&query=".rawurlencode($target)."\" class=\"key1\" return false>{$target}</a></td>
+		</td>
+	</tr>
+	<tr>
+ 		<td align='center'>
+		    <div id='menulayer_bgroup1' style='margin-top:-2px; margin-left:-30px; display:none; position:absolute;'>
+       			<table width='60' border='0' cellspacing='0' cellpadding='0' onmouseover=menulayer_bgroup1.style.display='';  onmouseout=menulayer_bgroup1.style.display='none'; >
+					<tr>
+						<td style='padding-top:5px;'>검색리스트</td>
+					</tr>
 	            </table>
 		    </div>
     	  </td>
@@ -107,34 +113,13 @@ function KeywordLink_bindKeyword($target,$mother) { //팝업 띄우면서 넘겨
   </tr>
 </table>
 	";
-
-
-/*
-	$target = "
-	<table>
-		<tr>
-			<td align='center' onmouseover="menulayer_bgroup1.style.display='';" onmouseout="menulayer_bgroup1.style.display='none';" >
-<a href=\"http://openapi.naver.com/search?key=".$apikey."&target=krdic&start=1&display=10&query=". rawurlencode($target) ."\" class= \" key1 \"  return false\">{$target}</a></td>
-		</tr>
-		<tr>
-			<td>
-				<div>
-				</div>
-			</td>	
-		</tr>
-	</table>
-
-	";
 */
+	$apikey = "6484ff3113728f5c49e7d921205d61a1";
+
+	$target = "<a href=\"http://openapi.naver.com/search?key=".$apikey."&target=krdic&start=1&display=10&query=". rawurlencode($target) ."\" class= \" key1 \"  return false\">{$target}</a>";
 
 	return $target;
 }
-
-function KeywordLink_insertJS(){
-	echo "";
-
-}
-
 
 function KeywordLink_setSkin($target,$mother) {
 	global $pluginPath;
@@ -148,4 +133,4 @@ function KeywordLink_setConfig($data){
        return true;
 }
 
-?> 
+?>
