0977e9ae795db2f792fcf27eb3b54c639999aa93
neighboryc <neighboryc@4fabb465-0037-0410-8b86-c3efdef5e10a>
2010-10-15 02:54:29 +0000

    dladudwoz.c file rewrite
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@449 4fabb465-0037-0410-8b86-c3efdef5e10a

--- oss2010cjnu/dladudwoz/dladudwoz.c
+++ oss2010cjnu/dladudwoz/dladudwoz.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	puts("It's a test file");
-
 	puts("My name is youngjae");
+
+	puts("Hi, My name is youngcheo.");
+	puts("Nice to meet you. ^^");
 
 	return 0;
 }
