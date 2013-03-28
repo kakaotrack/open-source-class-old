9bd28dc14d94bbc7e90f71a862f2f6bd9d5cbd20
oss2007cjnu@gmail.com <oss2007cjnu@gmail.com@4fabb465-0037-0410-8b86-c3efdef5e10a>
2013-03-28 11:56:26 +0000

    Refactoring small things
    git-svn-id: https://open-source-class.googlecode.com/svn/trunk@941 4fabb465-0037-0410-8b86-c3efdef5e10a

--- oss2013jnu/channy/hello.php
+++ oss2013jnu/channy/hello.php
@@ -1,7 +1,12 @@
 <?php
 	echo "Hello, world!";
-	if($_GET['year']) $year=$_GET['year']
-	else $year="2013";
-	
+
+	// if there is parameter 'year', show year!
+	if($_GET['year']) {
+		$year=$_GET['year'];
+	} else {
+		$year="2013";
+	}
+
 	echo "This year is ".$year.".";
 ?>
